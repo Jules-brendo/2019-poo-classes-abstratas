54a34b454ca9b9a75e1e8f2316ccd5c80c30a5eb
Jules-brendo <julesbreno@hotmail.com>
2019-10-09 14:33:05 -0300

    Add files via upload

---
+++ classes-abstratas/ContaPoupanca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class ContaPoupanca : Conta
+    {
+        public override void Deposita(double valorOperacao)
+        {
+            this.Saldo += valorOperacao;
+        }
+        public override void Saca(double valor)
+        {
+            this.Saldo -= (valor + 0.10);
+        }
+    }
+}
